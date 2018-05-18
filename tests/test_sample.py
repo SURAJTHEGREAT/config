341230232222e459410b7a343f3d8bcb362a229d
Suraj S <suraj.s@cognizant.com>
2018-05-18 09:16:35 -0400

    Added test case and rename package to config_suraj

---
+++ tests/test_sample.py
@@ -0,0 +1,16 @@
+import config.configuration as config
+from oslo_config import cfg
+def test_answer():
+
+    filename='test.conf'
+    db_opts = [
+        cfg.BoolOpt('db_enable',default=False),
+        cfg.StrOpt('host', default='mongo', help='host of db server'),
+        cfg.IntOpt('port', default=27017, help='port of db server')
+        ]
+
+    tag='database'
+    cfg.CONF=config.register_opts([filename],db_opts,tag)
+    assert cfg.CONF.database.host=="mongodb"
+    assert cfg.CONF.database.port==27017 
+
